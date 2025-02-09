0ae4271acda06dca8784d49d8e07a50cef0f42e7
Michal Ferber <michal.ferber@ari-integration.com>
2025-02-09 07:48:18 -0500

    v2025.02.09.001

--- obj/Debug/net9.0/IPCowV3.AssemblyInfo.cs
+++ obj/Debug/net9.0/IPCowV3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IPCowV3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dd05df002ffac33cdf2325b34bdc346bc872147")]
 [assembly: System.Reflection.AssemblyProductAttribute("IPCowV3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IPCowV3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
